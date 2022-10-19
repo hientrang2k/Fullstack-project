bdbe3b590fe645e6cf90ec3aae9df062de415ba9
rikkei-chiendv <chiendv@rikkeisoft.com>
2022-10-19 16:09:55 +0700

    hash password #25

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,5 +1,37 @@
-let createNewUser = (data) => {
-  console.log('Data from sevices: ', data);
+import bcrypt from 'bcryptjs';
+import db from '../models/index';
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashPassFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        gender: data.gender === '1' ? true : false,
+        typeRole: data.role,
+      });
+      resolve('ok! create success!');
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashPassword);
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 module.exports = {
